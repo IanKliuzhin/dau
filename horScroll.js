c55b6960876cca0460f9c95c0e4cbc952dddbf44
Yan Klyuzhin <klyuzhin@uchi.ru>
2019-10-05 06:05:49 +0300

    made pages-images scrollable

---
+++ horScroll.js
@@ -0,0 +1,18 @@
+const participantsAspRat = 5019 / 2133
+const instituteAspRat = 24219 / 2133
+
+document.addEventListener('DOMContentLoaded', () => {
+  const bg = document.getElementById("bg");
+  bg.addEventListener('wheel', (event) => {
+    const offsetPX = window.getComputedStyle(bg).getPropertyValue('background-position-x')
+    const offset = parseInt(offsetPX, 10)
+    const clientHeight = document.documentElement.clientHeight
+    const clientWidth = document.documentElement.clientWidth
+    const aspRat = bg.classList.contains('participantsBG') ? participantsAspRat : instituteAspRat
+    const imgWidth = parseInt(clientHeight * aspRat, 10)
+    const delta = event.wheelDelta
+    if (offset + delta > 0) bg.style.backgroundPositionX
+    else if (offset + delta + imgWidth < clientWidth) bg.style.backgroundPositionX = clientWidth - imgWidth
+    else bg.style.backgroundPositionX = offset + delta + 'px'
+  })
+});
