ab3b825b8f26b4fb88ff8c6ec2f85e6df4381be5
Yan Klyuzhin <klyuzhin@uchi.ru>
2019-11-22 03:15:47 +0300

    tried to fix hor scroll problems

--- helpers/horScroll.js
+++ helpers/horScroll.js
@@ -1,11 +1,12 @@
 (function() {
   function scrollHorizontally(e) {
       e = window.event || e;
-    //   console.log('e.wheelDelta', e.wheelDelta)
-    //   console.log('e.detail', e.detail)
-    //   var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-      var delta = Math.abs(e.wheelDelta) < 25 ? 3 * e.wheelDelta : e.wheelDelta;
-    //   console.log(delta)
+      // console.log('e.wheelDelta', e.wheelDelta)
+      // console.log('e.type', e.type)
+      // var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+      var delta = (Math.abs(e.wheelDelta) < 25 && Math.abs(e.wheelDelta) > 10) ? 2 * e.wheelDelta : e.wheelDelta;
+      // console.log(delta)
+      // console.log(e.wheelDelta, delta)
       document.documentElement.scrollLeft -= delta;
       document.body.scrollLeft -= delta;
       e.preventDefault();
