ee4f1cf41236b4f2cb1c10753f4e245c80c1903b
eugene <serebrennikov@uchi.ru>
2020-01-29 03:19:14 +0300

    about 2

--- helpers/lazyImagesLoader.js
+++ helpers/lazyImagesLoader.js
@@ -1,9 +1,6 @@
 export const lazyLoadImages = (doc) => {
   var lazyBackgrounds = [].slice.call(doc.querySelectorAll(".lazy"));
-  console.log("loader", doc)
-
   if ("IntersectionObserver" in window) {
-   console.log("loader", doc)
     const lazyBackgroundObserver = new IntersectionObserver(function(entries) {
       entries.forEach(function(entry) {
         if (entry.isIntersecting) {
